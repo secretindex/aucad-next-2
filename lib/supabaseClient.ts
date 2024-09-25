f37881a26bd79af869e246829c4295c3ed909082
secretindex <caio.smelo04@gmail.com>
2024-09-25 07:48:55 -0300

    prettier

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -4,4 +4,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
