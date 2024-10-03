20f27f3209aa7731d6f1ac307306cad73239c2bb
secretindex <caio.smelo04@gmail.com>
2024-10-03 17:25:43 -0300

    confirm email page (not used)

---
+++ app/auth/confirm-email/page.tsx
@@ -0,0 +1,10 @@
+export default function ConfirmEmail() {
+  return (
+    <section className="h-full flex flex-col justify-center items-center">
+      <div className="rounded-md p-2 shadow-md border-[1px] border-[#bebebe30]">
+        <h2>You're almost done!</h2>
+        <p>Please, check your email and then login.</p>
+      </div>
+    </section>
+  )
+}
