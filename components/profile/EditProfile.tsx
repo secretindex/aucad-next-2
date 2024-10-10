4d671c29b381c0c4fad4ad7b3ac8eea7fd6c7ced
secretindex <caio.smelo04@gmail.com>
2024-10-10 13:16:43 -0300

    make edit profile display user name

--- components/profile/EditProfile.tsx
+++ components/profile/EditProfile.tsx
@@ -1,6 +1,14 @@
-import { FC } from "react"
+import { BaseSyntheticEvent, FC, useState } from "react"
 
-const EditProfile: FC = () => {
+interface EditProps {
+  nome: string
+  sobrenome: string
+}
+
+const EditProfile: FC<EditProps> = ({ nome, sobrenome }) => {
+  const [name, setName] = useState<string>(nome)
+  const [lastName, setLastName] = useState<string>(sobrenome)
+
   return (
     <div className="flex flex-row gap-2 justify-evenly">
       <div className="w-full flex flex-col gap-2">
@@ -9,6 +17,8 @@ const EditProfile: FC = () => {
           type="text"
           name="first_name"
           id="first_name"
+          value={name}
+          onChange={(e: BaseSyntheticEvent) => setName(e.target.value)}
           className="border-[1px] border-[#bebebe30] focus:border-[#20202060] outline-none px-4 py-[0.3rem] rounded-md"
         />
       </div>
@@ -18,6 +28,8 @@ const EditProfile: FC = () => {
           type="text"
           name="last_name"
           id="last_name"
+          value={lastName}
+          onChange={(e: BaseSyntheticEvent) => setLastName(e.target.value)}
           className="border-[1px] border-[#bebebe30] focus:border-[#20202060] outline-none px-4 py-[0.3rem] rounded-md"
         />
       </div>
