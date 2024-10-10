6c935a83f557c7c449fba6b55b7624a9d8954506
secretindex <caio.smelo04@gmail.com>
2024-10-10 17:05:45 -0300

    deleting because route is still not used

--- app/api/fields/route.ts
+++
@@ -1,7 +0,0 @@
-export default async function POST(req: Request) {
-  try {
-    const { title, message } = await req.json()
-  } catch (e) {
-    return Response.json({ message: e, status: "fail" })
-  }
-}
