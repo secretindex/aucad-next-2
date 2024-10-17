f2bcb86e65be1b10333d17b0da58d1c240be793d
secretindex <caio.smelo04@gmail.com>
2024-10-17 17:08:35 -0300

    get and post census from api

--- app/api/census/route.ts
+++ app/api/census/route.ts
@@ -1,8 +1,18 @@
 import { createClient } from "@/lib/supabase/ssr/ssrServer"
+import { NextRequest } from "next/server"
 
-async function GET() {
+async function GET(req: NextRequest) {
   const supabase = createClient()
-  const { data, error } = await supabase.from("census").select("*")
+  try {
+    const id = req.nextUrl.searchParams.get("id")
+    const { data, error } = await supabase.from("census").select("*").eq("id", id);
+
+    if (error) throw new Error(error.message as string)
+
+    return Response.json({ data, status: "success" })
+  } catch (e) {
+    return Response.json({ message: e, status: "fail"})
+  }
 }
 
 async function POST(req: Request) {
@@ -13,11 +23,13 @@ async function POST(req: Request) {
     const { data, error } = await supabase.from("census").insert(body)
 
     console.log(body)
+    console.log(error)
 
     if (error) throw new Error(error.message)
 
     return Response.json({ response: data, originalBody: body, data: body })
   } catch (e) {
+    console.log(e)
     return Response.json({ message: e, status: "fail" })
   }
 }
