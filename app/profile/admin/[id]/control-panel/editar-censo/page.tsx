dc7ff6fd3ab720cae95c9006040c183a4da15701
secretindex <caio.smelo04@gmail.com>
2024-10-23 15:16:41 -0300

    todo: all censos page

---
+++ app/profile/admin/[id]/control-panel/editar-censo/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const EditarCenso = () => {
+  const [censoList, setCensoList] = useState<Array<any>>([])
+
+  const getCensos = async () => {
+    const censos = await axios.get("/api/census/get-all-census")
+    console.log(censos.data)
+  }
+
+  useEffect(() => {
+    getCensos()
+  }, [])
+
+  return (
+    <section>
+      <div>
+        <h2>Editar Censo</h2>
+      </div>
+      <div>
+        <ul>
+          {censoList.map((censo) => (
+            <li key={censo.id}>
+              {censo.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default EditarCenso
