2029a4de242999b84676c97da243677ca3246212
secretindex <caio.smelo04@gmail.com>
2024-12-11 15:27:58 -0300

    associate user with census

--- app/api/census/insert-users/route.ts
+++ app/api/census/insert-users/route.ts
@@ -1,15 +1,18 @@
 import { createClient } from "@/lib/supabase/ssr/ssrServer"
 
+// insert census users
 async function POST(req: Request) {
   const supabase = createClient()
   try {
     const usersCensuses = await req.json()
 
-    // const { error } = await supabase
-    //   .from("profiles_censuses")
-    //   .insert(usersCensuses)
+    console.log("This is from profile censuses", usersCensuses)
 
-    // if (error) throw new Error(error.message)
+    const { error } = await supabase
+      .from("profiles_censuses")
+      .insert(usersCensuses)
+
+    if (error) throw new Error(error.message)
 
     return Response.json({ message: usersCensuses, status: "ok" })
   } catch (e) {
