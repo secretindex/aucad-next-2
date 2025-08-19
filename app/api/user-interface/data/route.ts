8d110df1acf6e1adf9388cd4df05c9738a6d6148
secretindex <caio.smelo04@gmail.com>
2025-08-19 14:46:58 -0300

    fixed empty route file

---
+++ app/api/user-interface/data/route.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@/lib/supabase/ssr/ssrServer"
+import { NextRequest } from "next/server"
+
+// TODO: PROVISÓRIO
+
+async function GET(req: NextRequest) {
+  const supabase = createClient()
+  try {
+    const user_id = req.nextUrl.searchParams.get("user_id")
+
+    const { data, error } = await supabase.from("profiles_censuses").select("census(*)").eq("profile_id", user_id)
+
+    if (error) throw new Error(error.message)
+
+    return Response.json({ message: data, status: "ok" })
+  } catch (e) {
+    return Response.json({ message: "Você não possui acesso a nenhum censo. Fale com um administrador.", error: e, status: "fail" })
+  }
+}
+
+export { GET }
